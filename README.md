e9f136d225de55b02a50c415d5076981b012519c
anton.shcherbyna <anton.shcherbyna@sigma.software>
2019-01-23 18:18:38 +0200

    add typescript

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # telegram-bot
+### Use cases
+* Add user to the queue
+* group moderator: remove all users from queue, ask for next user
--- index.js
+++
@@ -1,7 +0,0 @@
-const botgram = require("botgram")
-const bot = botgram("618906845:AAEbB4nDBfuQftzJEPf5rcIco0ZWmwW_1I8");
-bot.command("start", "help", (msg, reply) =>
-  reply.text("To schedule an alert, do: /alert <seconds> <text>"))
-bot.text(function (msg, reply, next) {
-    console.log("Received a text message:", msg.text);
-  });
